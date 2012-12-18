561d964e91c7839681192285c0bacd2126a70d2f
Chris Bailey <c.bailey@bristol.ac.uk>
2012-12-18 15:53:12 +0000

    Modifying so that: 1. Downloads appear inside chrome 2. Filename generated from url 3. Only render local print.css files. This enables more control over the pisa formatting (e.g. we can still have externally referencable CSS3 files in our app)

--- django_pdf/middleware.py
+++ django_pdf/middleware.py
@@ -1,41 +1,39 @@
 #!/usr/bin/env python
 
 import os.path
+import xhtml2pdf.pisa as pisa
 import cStringIO as StringIO
 import cgi
-import ho.pisa as pisa
-import cStringIO as StringIO
-import cgi
+from django import http
 from django.http import HttpResponse
 from django.conf import settings
 
 REQUEST_FORMAT_NAME = getattr(settings, 'REQUEST_FORMAT_NAME', 'format')
 REQUEST_FORMAT_PDF_VALUE = getattr(settings, 'REQUEST_FORMAT_PDF_VALUE', 'pdf')
-TEMPLATE_PDF_CHECK = getattr(settings, 'TEMPLATE_PDF_CHECK',
-'DJANGO_PDF_OUTPUT')
+TEMPLATE_PDF_CHECK = getattr(settings, 'TEMPLATE_PDF_CHECK', 'DJANGO_PDF_OUTPUT')
 
 
 def fetch_resources(uri, rel):
     """
-    Prepares paths for pisa
+    Prepares paths for pisa, only includes local print.css files
     """
-    path = os.path.join(settings.MEDIA_ROOT,
-            uri.replace(settings.MEDIA_URL, ""))
+    path = ''
+    if 'http' not in uri and 'print.css' in uri:
+        path = os.path.join(settings.STATIC_ROOT,
+                uri.replace(settings.STATIC_URL, ""))
     return path
 
 
-def transform_to_pdf(response):
-    response['mimetype'] = 'application/pdf'
-    response['Content-Disposition'] = 'attachment; filename=report.pdf'
-    content = response.content
+def transform_to_pdf(response, name):
+    content = response.content.encode("UTF-8")
     result = StringIO.StringIO()
-    pdf = pisa.pisaDocument(StringIO.StringIO(content),
-            response, link_callback=fetch_resources)
+    pdf = pisa.pisaDocument(StringIO.StringIO(content), result, link_callback=fetch_resources)
     if not pdf.err:
+        response = http.HttpResponse(result.getvalue(), mimetype='application/pdf')
+        response['Content-Disposition'] = 'filename=%s.pdf' % name
         return response
     else:
-        return http.HttpResponse('We had some errors<pre>%s</pre>' %
-                cgi.escape(html))
+        return http.HttpResponse('We had some errors<pre>%s</pre>' % cgi.escape(html))
 
 
 class PdfMiddleware(object):
@@ -45,5 +43,6 @@ class PdfMiddleware(object):
     def process_response(self, request, response):
         format = request.GET.get(REQUEST_FORMAT_NAME, None)
         if format == REQUEST_FORMAT_PDF_VALUE:
-            response = transform_to_pdf(response)
+            path = request.path
+            response = transform_to_pdf(response, path[path.rfind('/')+1:])
         return response
