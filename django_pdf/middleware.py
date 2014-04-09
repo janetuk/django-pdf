c4ff89d82aecab5ce97fd8e61e5cc55fc82ecb89
Chris Bailey <c.bailey@bristol.ac.uk>
2014-04-09 11:52:24 +0100

    Adding explicit method to obtain filename from url string to better handle edge cases

--- django_pdf/middleware.py
+++ django_pdf/middleware.py
@@ -13,6 +13,7 @@ REQUEST_FORMAT_PDF_VALUE = getattr(settings, 'REQUEST_FORMAT_PDF_VALUE', 'pdf')
 TEMPLATE_PDF_CHECK = getattr(settings, 'TEMPLATE_PDF_CHECK', 'DJANGO_PDF_OUTPUT')
 PHANTOMJS_EXECUTABLE = getattr(settings, 'PHANTOMJS_EXECUTABLE', 'phantomjs')
 
+
 def transform_to_pdf(response, host='', filename='page.pdf'):
     """
     This function writes the html to a temp file and passes it to PhantomJS
@@ -21,45 +22,57 @@ def transform_to_pdf(response, host='', filename='page.pdf'):
     """
     # create a temp file to write our HTML to
     input_file = tempfile.NamedTemporaryFile(delete=False)
-    
+
     # insert base so that static resources are loaded correctly
     content = response.content.decode("UTF-8").encode("UTF-8")
-    
+
     if host != 'http://testserver/':
         content = content.replace('<head>','<head><base href="%s">' % host)
-    
+
     input_file.write(content)
     input_file.close()
-    
+
     # construct parameters to our phantom instance
     args = [PHANTOMJS_EXECUTABLE,
             os.path.dirname(__file__)+"/html2pdf.js",
             input_file.name,
             input_file.name+'.pdf']
-    
+
     # create the process
     p = call(args)
-    
+
     # read the generated pdf output
     output_file = open(input_file.name+'.pdf','rb')
-    
+
     contents = output_file.read()
-    
+
     # delete the files
     output_file.close()
     os.remove(input_file.name)
     os.remove(output_file.name)
-    
+
     # return contents to browser with appropriate mimetype
     response = HttpResponse(contents, content_type='application/pdf')
-    
+
     # Add extension if missing
     if not filename.endswith('.pdf'):
         filename = "%s.pdf" % filename
-    
+
     response['Content-Disposition'] = 'filename=%s' % filename
     return response
-    
+
+def get_filename(path):
+    """
+    Returns a string representation of the url path to be used as a filename
+    """
+    if path.find('?') > 0:
+        path = path[:path.find('?')]
+    if path.endswith('/'):
+        path = path[:-1]
+    filename = path[path.rfind('/')+1:]
+    if not filename:
+        filename = 'page'
+    return filename+'.pdf'
 
 class PdfMiddleware(object):
     """
@@ -68,8 +81,9 @@ class PdfMiddleware(object):
     def process_response(self, request, response):
         format = request.GET.get(REQUEST_FORMAT_NAME, None)
         if format == REQUEST_FORMAT_PDF_VALUE:
-            path = request.path
-            if path.endswith('/'):
-                path = path[:-1]
-            response = transform_to_pdf(response, request.build_absolute_uri('/'), path[path.rfind('/')+1:])
+            filename = get_filename(request.path)
+            response = transform_to_pdf(response,
+                                        request.build_absolute_uri('/'),
+                                        filename)
         return response
+
--- django_pdf/tests/pdf_generation.py
+++ django_pdf/tests/pdf_generation.py
@@ -7,7 +7,8 @@ from os import listdir
 from django.test import TestCase
 from django.http import HttpResponse
 
-from django_pdf.middleware import transform_to_pdf
+from django_pdf.middleware import transform_to_pdf, get_filename
+
 
 class TestPDFGeneration(TestCase):
     @classmethod
@@ -19,7 +20,7 @@ class TestPDFGeneration(TestCase):
                 <p>%s</p>
                 </body>
                 </html>""" % cls.string_contents
-        
+
         # Create a stubbed response object
         cls.response = HttpResponse(html)
 
@@ -27,19 +28,19 @@ class TestPDFGeneration(TestCase):
         """ Check we can create a simple pdf file """
         # create our pdf
         response = transform_to_pdf(self.response)
-        
+
         # Check the generated pdf contains our contents
         pdf_content = get_content_from_pdf(response.content)
         assert self.string_contents in pdf_content
-    
+
     def test_filename_handing(self):
         """ Checks the filename handling in the Content-Disposition header """
         # Check a default name is provided
         response = transform_to_pdf(self.response)
         assert 'page.pdf' in response['Content-Disposition']
-        
+
         # Check we can provide our own filename
-        response = transform_to_pdf(self.response,'','testfile')
+        response = transform_to_pdf(self.response, '', 'testfile')
         assert 'testfile.pdf' in response['Content-Disposition']
 
     def test_temporary_file_cleanup(self):
@@ -47,12 +48,12 @@ class TestPDFGeneration(TestCase):
         Checks that all temporary files are cleaned up after generating the pdf
         """
         temp_files_before = len(listdir(tempfile.gettempdir()))
-        response = transform_to_pdf(self.response)
+        transform_to_pdf(self.response)
         temp_files_after = len(listdir(tempfile.gettempdir()))
         assert temp_files_before == temp_files_after
 
     def test_character_encodings(self):
-        """ 
+        """
         Tests that the pdf generation can support various character sets for 
         different alphabets
         """
@@ -68,13 +69,13 @@ class TestPDFGeneration(TestCase):
                     ISO-2022-JP - ゴシック (Japanese ja)
                 </body>
                 </html>"""
-        
+
         response = HttpResponse(html)
         response = transform_to_pdf(response)
-        
+
         # Check the generated pdf contains our contents
         pdf_content = get_content_from_pdf(response.content)
-        
+
         assert u'чуоюющяа' in pdf_content
         assert u'ﻗﻠﯿﻼ' in pdf_content
         assert u'ΔΦψ' in pdf_content
@@ -82,21 +83,29 @@ class TestPDFGeneration(TestCase):
         assert u'Ṫêċï' in pdf_content
         assert u'電电' in pdf_content
         assert u'ゴシック' in pdf_content
-        
+
         assert u'\x00' not in pdf_content
+
+    def test_get_filename_method(self):
+        assert get_filename('') == 'page.pdf'
+        assert get_filename('/') == 'page.pdf'
+        assert get_filename('/test') == 'test.pdf'
+        assert get_filename('/test?a=etc') == 'test.pdf'
+        assert get_filename('/test/') == 'test.pdf'
+        assert get_filename('/test/?a=etc') == 'test.pdf'
 
 
 def get_content_from_pdf(content):
     """
     Helper function to extract text from pdf using the slate library
     """
-    
+
     output = StringIO.StringIO()
     output.write(content)
-    
+
     doc = ''
     for page in slate.PDF(output):
         doc = doc + page.decode("UTF-8").replace('\t',' ')
     output.close()
-    
+
     return doc
